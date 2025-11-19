5272801e5ab7393c07343d0b13b4251112d6f116
Rizal Anggoro <gnoogler4@gmail.com>
2025-11-19 09:41:28 +0700

    feat: improve error handling in Failure.kt with detailed messages

--- app/src/main/java/id/my/smartdoorlock/core/extensions/Failure.kt
+++ app/src/main/java/id/my/smartdoorlock/core/extensions/Failure.kt
@@ -10,8 +10,8 @@ fun String.toFailure(): Failure = try {
     if (this.isNotEmpty())
         Gson().fromJson(this, Failure::class.java)
     else
-        Failure(message = "Terjadi kesalahan tak terduga!")
+        Failure(message = "Terjadi kesalahan tak terduga! empty")
 } catch (e: Exception) {
     e.printStackTrace()
-    Failure(message = "Terjadi kesalahan tak terduga!")
+    Failure(message = "Terjadi kesalahan tak terduga! ${e.message}")
 }
